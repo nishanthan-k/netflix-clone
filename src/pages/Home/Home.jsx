032625e281e0baa8b2e7fe0938407c20644eec86
nishanthan-k <nishanthank9102@gmail.com>
2023-12-31 16:31:27 +0530

    added navbar

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../../components/NavBar/NavBar"
+import "./Home.scss"
+
+const Home = () => {
+  return (
+    <div className="home">
+      <NavBar />
+    </div>
+  )
+}
+
+export default Home
